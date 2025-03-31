ae5d7998a6d598455733f019986de5780dfc82eb
Paulo Eremita <eremitaio79@gmail.com>
2025-03-30 21:59:24 -0300

    AuthController finalizado
    AuthController finalizado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+        echo "Homepage";
+    }
+
+
+    public function newNote() {
+        echo "Criando uma nova nota.";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\VerificaSeNaoEstaLogado;
+use App\Http\Middleware\VerificaSeUsuarioEstaLogado;
 use Illuminate\Support\Facades\Route;
 
-// Rotas de acesso (Auth).
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/login-submit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::middleware([VerificaSeNaoEstaLogado::class])->group(function() {
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/login-submit', [AuthController::class, 'loginSubmit']);
+});
+
+
+Route::middleware([VerificaSeUsuarioEstaLogado::class])->group(function() {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/new-note', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+
